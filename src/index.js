8db3f8fbe8320e697b5d0a538fc8e5566181f9ea
YuliiaTsova <yuliiatsova@gmail.com>
2024-01-12 10:04:22 -0400

    gh-pages settings

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { store } from './redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  <BrowserRouter>
+  <BrowserRouter basename="/comIT-FrontEnd-Library">
     <Provider store={store}>
       <App />
     </Provider>
